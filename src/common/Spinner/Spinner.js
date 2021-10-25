ca6b98154712232df312b385a4a9ea99bea36140
Nikita Uhlin <nikitauhlin@yandex.ru>
2021-10-25 14:05:33 +0300

    V-03+F-04 Правки по коду

--- src/common/Spinner/Spinner.js
+++ src/common/Spinner/Spinner.js
@@ -1,21 +1,20 @@
 import React from "react";
 import styles from "./spinner.module.sass";
 
-const Spinner = () => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.content}>
+const Spinner = () => (
+    <div className={styles.container}>
+        <div className={styles.content}>
+            <div></div>
+            <div></div>
+            <div>
                 <div></div>
+            </div>
+            <div>
                 <div></div>
-                <div>
-                    <div></div>
-                </div>
-                <div>
-                    <div></div>
-                </div>
             </div>
         </div>
+    </div>
 
-    );
-};
+)
+
 export default Spinner;
